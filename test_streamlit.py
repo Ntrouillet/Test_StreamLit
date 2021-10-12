e37dc5de191f90492ef024734325d6993c3f6646
Ntrouillet <92300716+Ntrouillet@users.noreply.github.com>
2021-10-12 11:30:36 +0200

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -14,13 +14,13 @@ img_bug = cv2.imread('Etiquette_sourire_info_bug.png')
 #Chemin de Tesseract
 # pytesseract.pytesseract.tesseract_cmd = r'.`\Tesseract\tesseract.exe'
 
-st.image([img_correct, img_bug], width = 300)
-
 #Config de tesseract
 custom_config = '--oem 1 --psm 7'
 
 #Titre
 st.title('TEST ETIQUETAGE')
+
+st.image([img_correct, img_bug], width = 300)
 
 type_image = st.radio("Quelle image tester",('Avec la date', 'Sans la date'))
 
